3eeb175fa7b579efc17a8ca85e5a978eff4d76b2
@ <yuezhaoyang7449@163.com>
2017-03-11 16:31:17 +0800

    update networkrequest

--- app/src/main/java/com/example/y/mvp/network/NetWorkRequest.java
+++ app/src/main/java/com/example/y/mvp/network/NetWorkRequest.java
@@ -4,6 +4,7 @@ import com.example.y.mvp.mvp.Bean.BaseBean;
 import com.example.y.mvp.mvp.Bean.NewsDetailInfo;
 import com.example.y.mvp.utils.RxUtils;
 
+import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -13,55 +14,38 @@ import rx.schedulers.Schedulers;
  */
 public class NetWorkRequest {
 
-
-    public static void newsDetail(int id, Subscriber<NewsDetailInfo> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getNewsDetail(id)
-                .subscribeOn(Schedulers.io())
+    private static <M> void addObservable(Observable<M> observable, Subscriber<M> subscriber) {
+        RxUtils.getInstance().addSubscription(observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(subscriber));
+    }
+
+    public static void newsDetail(int id, Subscriber<NewsDetailInfo> subscriber) {
+        addObservable(Network.getTngouApi().getNewsDetail(id), subscriber);
     }
 
     public static void newsList(int id, int page, Subscriber<BaseBean.NewsListBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getNewsList(id, page)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getNewsList(id, page), subscriber);
     }
 
     public static void tabNews(Subscriber<BaseBean.TabNewsBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getTabNews()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getTabNews(), subscriber);
     }
 
     public static void imageDetail(int id, Subscriber<BaseBean.ImageDetailBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getImageDetail(id)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getImageDetail(id), subscriber);
     }
 
     public static void imageList(int id, int page, Subscriber<BaseBean.ImageListBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getImageList(id, page)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getImageList(id, page), subscriber);
     }
 
     public static void imageNew(int id, int rows, Subscriber<BaseBean.ImageNewBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getImageNews(id, rows)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getImageNews(id, rows), subscriber);
     }
 
     public static void tabName(Subscriber<BaseBean.TabNameBean> subscriber) {
-        RxUtils.getInstance().addSubscription(Network.getTngouApi().getTabName()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber));
+        addObservable(Network.getTngouApi().getTabName(), subscriber);
     }
 
 }
